3373b05d2753fddb4adb1a6aaa8a0a557565733b
opxarch <diyer175@hotmail.com>
2019-01-05 00:33:06 +0800

    support for hardware debug ports

---
+++ debug.c
@@ -0,0 +1,15 @@
+
+#include "openpx64k-emu.h"
+#include <stdio.h>
+
+
+static char enable_debug = 1;
+
+void debug_putc(uint8_t ch)
+{
+  if(enable_debug)
+    {
+      putchar(ch);
+      fflush(stdout);
+    }
+}
